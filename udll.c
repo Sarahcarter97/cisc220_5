887f14629d4a2c2d387b62f0b113d12d71aeee0c
Sarahcarter97 <15sca@queensu.ca>
2016-12-01 16:33:37 -0500

    Update udll.c

--- udll.c
+++ udll.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include"udll.h"
 
 /* creates a new node and inserts it into the specified index. 
 Connects the previous node to it and connect itself to the next node */
@@ -7,9 +9,11 @@ void insert(int index, union Data data){
 /* deletes node in specific index. Connects the previous node to the next node*/
 void remove (int index){
 }
+/* returns the data value of the node of the specific index*/ 
 
 union Data get(int index){
 }
+/*returns the number of nodes in the list*/
 
 int length(){
 }
