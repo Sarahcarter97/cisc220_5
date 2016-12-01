ca34cf71f3a20dd4d8b6796963c3302c31b550cf
Sarahcarter97 <15sca@queensu.ca>
2016-12-01 16:24:49 -0500

    Update udll.c

--- udll.c
+++ udll.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
 
+/* creates a new node and inserts it into the specified index. 
+Connects the previous node to it and connect itself to the next node */
+void insert(int index, union Data data){
+}
+
+void remove (int index){
+}
+
+union Data get(int index){
+}
+
+int length(){
+}
+
+  
+  
+  
+  
+  
+  
+ 
