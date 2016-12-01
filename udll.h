771611d244c611c858e2cfbc2ffff796a2785576
14drwr <14drwr@queensu.ca>
2016-12-01 17:08:13 -0500

    Update udll.h

--- udll.h
+++ udll.h
@@ -1,1 +1,24 @@
+#ifndef UDLL_H
+#define UDLL_H
 
+union Data {
+  int i;
+  int *iPtr;
+  float f;
+  float *fPtr;
+  char c;
+  char *cPtr;
+};
+
+typedef scruct Node {
+  struct Node *next;
+  struct Node *previous;
+  union Data data;
+} Node;
+
+void linkedList_insert(int index, union Data data);
+void linkedList_remove(int index);
+union Data linkedList_get(int index);
+int linkedList_length();
+
+#endif
