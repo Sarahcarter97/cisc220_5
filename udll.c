3dc2ef33fced9773afe16e97738a1c756caaab38
Sarahcarter97 <15sca@queensu.ca>
2016-12-01 16:35:42 -0500

    Update udll.c

--- udll.c
+++ udll.c
@@ -7,7 +7,7 @@ Connects the previous node to it and connect itself to the next node */
 void insert(int index, union Data data){
 }
 /* deletes node in specific index. Connects the previous node to the next node*/
-void remove (int index){
+void removeNode (int index){
 }
 /* returns the data value of the node of the specific index*/ 
 
