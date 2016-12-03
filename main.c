73dbd1bd386d6ec8b80ed200a48713e94bb6ad75
14drwr <14drwr@queensu.ca>
2016-12-02 20:51:24 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "udll.h"
 
+int main(){
+        union Data testData;
+        testData.i = 6;
+  
+        insert(9,testData);
+        printf("Length before removal: %d\n", length());
+  
+        removeNode(1);
+        printf("Length after removal: %d\n", length());
+        
+        printf("First value: %d\n", get(0));
+  
+        return 0;
+}
