e52e485d38e2d9a4452ee78b7b6d27c4059e05b8
14drwr <14drwr@queensu.ca>
2016-12-02 20:56:55 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+//Assignment 5
+//Sarah Carter 10193595
+//Katherine DuQuesnay 10138898
+//Adele McCallum 10093867
+//Dean Wilkins-Reeves 10176758
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "udll.h"
